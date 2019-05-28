d90de2511ada483349c4b87b21eebca4d7dcaa0a
Mikkel Ebjerg <mlebjerg@me.com>
2019-05-28 02:09:40 +0200

    Core 2.2 instead of 2.1

---
+++ CodeDocs-Challenge/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeDocs_Challenge.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
